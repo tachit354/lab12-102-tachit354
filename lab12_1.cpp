b8d8427eae750d9b301cc0ebc08df111597d01c6
tachit354 <tachit_th@cmu.ac.th>
2025-12-30 15:21:14 +0700

    finish

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,18 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+int fibonacci(int x){
+    if(x == 0) return 0;
+    if(x == 1) return 1;
+    if (x < 1) return 0;
+    else return fibonacci(x-1) + fibonacci(x-2);
 }
